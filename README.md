4158b686b0f5eba6052325a7dab8e1865d4f0b4f
Maged Abdelsalam <dagadego@gmail.com>
2020-09-17 06:15:38 -0700

    Added save score feature

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # code-quiz
 04 Code Quiz
+Test your knowledge of Dune and record your score!
+
+## Links
+* [Github repository](https://github.com/magedabdelsalam/code-quiz)
+* [Website](https://magedabdelsalam.com/code-quiz/index.html)
--- script.js
+++ script.js
@@ -10,105 +10,114 @@
 // - Display score and save it
 
 var timer = 30;
+var name = []
 var score = 0;
-var qNa = {
-    "questionOne":{
-        question: "first question",
-        options: [
-            "wrong",
-            "wrong",
-            "wrong",
-            "apple",
-        ],
-        answer: "apple"
-    },
-    "questionTwo":{
-        question: "second question",
-        options: [
-            "wrong",
-            "okokok",
-            "wrong",
-            "wrong",
-        ],
-        answer: "okokok"
-    },
-    "questionThree":{
-        question: "third question",
-        options: [
-            "wrong",
-            "wrong",
-            "potato",
-            "wrong",
-        ],
-        answer: "potato"
+var currentQuestion = 0;
+
+var storedName = JSON.parse(localStorage.getItem("name"));
+var storedScore = JSON.parse(localStorage.getItem("score"));
+
+var questionOne = ["What does Arrakis mean?","A dog","A rabbit","A planet","A man","A planet"];
+var questionTwo = ["What happens when shield and lazer gun interact?","Explode","Deflect","Penetrate","Nothing","Explode"];
+var questionThree = ["What house is Paul from?","Emperor","Fremen","Harkonnen","Bene gesserit","Bene gesserit"];
+var questions = [questionOne,questionTwo,questionThree];
+
+for(var i=0;i<questions.length;i++){
+    var questionDiv= $("<div>");
+    questionDiv.attr("style","display: none");
+    questionDiv.attr("id",i);
+    questionDiv.attr("data-question","#"+i);
+    questionDiv.attr("class","quiz-question col-12 mx-auto mb-3");
+    $("#main").append(questionDiv);
+    for(var j=0;j<5;j++){
+        if (j===0){
+            var questionH = $("<h1>");
+            questionH.attr("class","question");
+            $(questionDiv).append(questionH);
+            questionH.text(questions[i][j]);
+        } else {
+            var questionP = $("<button>");
+            questionP.attr("class","option btn btn-secondary btn-lg btn-block");
+            $(questionDiv).append(questionP);
+            questionP.text(questions[i][j]);
+        }
     }
 }
 
+var gameOverDiv= $("<div>");
+gameOverDiv.attr("style","display: none");
+gameOverDiv.attr("class","quiz-over col-12 mx-auto mb-3");
+$("#main").append(gameOverDiv);
+
+var gameOverH = $("<h1>");
+$(gameOverDiv).append(gameOverH);
+gameOverH.text("Game over");
+
+var gameOverP = $("<p>");
+$(gameOverDiv).append(gameOverP);
+
+var gameOverForm = $("<form>");
+gameOverForm.attr("class","form-group");
+$(gameOverDiv).append(gameOverForm);
+
+var gameOverInput = $("<input>");
+gameOverInput.attr("class","form-control");
+$(gameOverForm).append(gameOverInput);
+
+var scoreP = $("<h2>");
+$(gameOverDiv).append(scoreP);
+gameOverForm.submit(function(event){
+    event.preventDefault()
+    name = gameOverInput.val();
+    storeName = localStorage.setItem("name",JSON.stringify(name));
+    storeScore = localStorage.setItem("score",JSON.stringify(score));
+    scoreP.text(name + " - " +score);
+});
+scoreP.text(storedName + " - " +storedScore);
+
+
+var timerDiv= $("<div>");
+timerDiv.attr("style","display: none");
+timerDiv.attr("class","quiz-timer col-12 mx-auto mb-3");
+$("#main").append(timerDiv);
+
+var timerH = $("<h4>");
+$(timerDiv).append(timerH);
+timerH.text(timer);
+
 $("#start-quiz").click(function(){
 
-    $("#quiz-body").empty();
+    $("#quiz-body").attr("style","display: none");
+    $(".quiz-timer").attr("style","display: block");
+    $("#0").attr("style","display: block");
 
-    var question = $("<h1>");
-    $("#quiz-body").append(question);
-    question.text(qNa.questionOne.question);
-    for(var i=0;i<qNa.questionOne.options.length;i++){
-        var options = $("<h3>");
-        $("#quiz-body").append(options);
-        options.text(qNa.questionOne.options[i]);
-    }
-    var timerDisplay = $("<h4>");
-    $("#quiz-body").append(timerDisplay.text(timer));
-
-    var countDown = setInterval(function(){ 
-        timer = timer - 1;
+    var timeLeft = setInterval(function(){ 
         console.log(timer);
-        if (timer <= 0){
-            console.log("it's working");
-            clearInterval(countDown);
-            $("#quiz-body").empty();
-            var gameOver = $("<h1>Game over</h1>");
-            $("#quiz-body").append(gameOver);
-            var tryAgain = $("<button id='start-quiz' class='btn btn-primary'>Try again?</button>");
-            $("#quiz-body").append(tryAgain);
-            
+        if (timer <= 0 || score === questions.length){
+            $(".quiz-timer").attr("style","display: none");
+            $(".quiz-question").attr("style","display: none");
+            $(".quiz-over").attr("style","display: block");
+            gameOverP.text("Your final score is " + score + ". Enter your name to record your score.");
+            clearInterval(timeLeft);
         }
-        timerDisplay.text(timer)
+        $(".quiz-timer h4").text(timer);
+        timer = timer - 1;
     }, 1000);
 
-    $("h3").click(function(){
-
-        if($(this).text() === qNa.questionOne.answer){
-            $("#quiz-body").empty();
-            var question = $("<h1>");
-            $("#quiz-body").append(question);
-            question.text(qNa.questionTwo.question);
-            for(var i=0;i<qNa.questionTwo.options.length;i++){
-                var options = $("<h3>");
-                $("#quiz-body").append(options);
-                options.text(qNa.questionTwo.options[i]);
-            }
-        } else if ($(this).text() !== qNa.questionOne.answer){
-            timer = timer - 10;
-            console.log("Reduce " + timer + " second")
-        }
-
-        if($(this).text() === qNa.questionTwo.answer){
-            console.log("yo it's wokrin")
-            $("#quiz-body").empty();
-            var question = $("<h1>");
-            $("#quiz-body").append(question);
-            question.text(qNa.questionThree.question);
-            for(var i=0;i<qNa.questionThree.options.length;i++){
-                var options = $("<h3>");
-                $("#quiz-body").append(options);
-                options.text(qNa.questionThree.options[i]);
-            }
-        } else if ($(this).text() !== qNa.questionOne.answer){
-            timer = timer - 10;
-            console.log("Reduce " + timer + " second")
-        }
-
-
-    })
-
 });
+    
+$(".option").click(function(){
+    if($(this).text() === questions[currentQuestion][5]){
+        var that = $(this).parent().attr("data-question");
+        $(that).attr("style","display: none");
+        $(that).next().attr("style","display: block");
+        score++;
+        console.log(score);
+        currentQuestion++;
+    } else if($(this).text() != questions[currentQuestion][5]){
+        timer = timer - 10;
+    }
+});
+
+
+
